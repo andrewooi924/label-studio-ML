d23f8afbc61bf3ee143d57888156448c60f62739
Wei Chun <tanweichun@outlook.com>
2023-05-17 21:56:54 +0800

    restructure some tests

--- tests/integration/test_gcs_util.py
+++ tests/integration/test_gcs_util.py
@@ -3,7 +3,7 @@ import tempfile
 
 import pytest
 from google.cloud import storage
-from utils.gcs import download_public_file, get_metadata_from_url
+from utils.gcs import download_public_file
 
 BUCKET = "ucf-crime-dataset"
 TEST_VIDEO = "Assault/Assault038_x264.mp4"
@@ -19,8 +19,3 @@ def test_download_public_file(destination_file_name):
     download_public_file(BUCKET, TEST_VIDEO, destination_file_name)
     assert os.path.exists(destination_file_name)
     assert os.path.getsize(destination_file_name) > 0
-
-
-def test_get_metadata_from_url():
-    url = f"gs://{BUCKET}/{TEST_VIDEO}"
-    assert get_metadata_from_url(url) == (BUCKET, TEST_VIDEO, "Assault038_x264.mp4")
